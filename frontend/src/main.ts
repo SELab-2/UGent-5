796e804756559acf2309b196f74feb937b3ff9aa
Marieke <marieke.sinnaeve@ugent.be>
2024-02-25 17:39:14 +0100

    login frontend when opening

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,11 +4,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import LoginView from './views/LoginView.vue'
 import router from './router'
 
 const app = createApp(App)
+const login = createApp(LoginView)
 
-app.use(createPinia())
-app.use(router)
+login.use(createPinia())
+login.use(router)
 
-app.mount('#app')
+login.mount('#app')
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login">
+    <img alt="white logo" class="logo" src="@/assets/logo_white_ transparant.png"  height="100" />
+    <h3> De officiële indienapplicatie van de Universiteit Gent</h3>
+    <p> Aanmelden</p>
+    <a class="login-button"> Apollo login </a>
+    <img alt="ugent logo" class="ugent-logo" src="@/assets/universiteit-gent-logo-white.png"  height="50" />
+  </div>
+</template>
+
+<script>
+import '../assets/login.css'
+
+export default {
+name: "LoginView"
+}
+</script>
+
+<style scoped>
+
+</style>
