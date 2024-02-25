42dba96338b2daccb7a4dbabbde948547735865b
Bram Reyniers <55666730+reyniersbram@users.noreply.github.com>
2024-02-25 11:10:08 +0100

    improve dev.sh

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export API_ENV=develpoment
+export API_ENV="develpoment"
 uvicorn app:app --reload --port 8080 \
     --ssl-keyfile "local-cert/localhost-key.pem" \
     --ssl-certfile "local-cert/localhost.pem"
