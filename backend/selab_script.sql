dba9b7a5c20f1f27b2cd095e1a15fa1e9ce403bf
drieshuybens <huybensdries@gmail.com>
2024-03-03 13:00:04 +0100

    Update selab_script.sql
    nr engels, snake_case azureobjectid -> uid

--- backend/selab_script.sql
+++ backend/selab_script.sql
@@ -1,139 +1,116 @@
-DROP TABLE IF EXISTS Indiening CASCADE;
-DROP TABLE IF EXISTS Status CASCADE;
-DROP TABLE IF EXISTS Project CASCADE;
-DROP TABLE IF EXISTS StudentGroep CASCADE;
-DROP TABLE IF EXISTS Groep CASCADE;
-DROP TABLE IF EXISTS StudentVak CASCADE;
-DROP TABLE IF EXISTS LesgeverVak CASCADE;
-DROP TABLE IF EXISTS Vak CASCADE;
-DROP TABLE IF EXISTS WebsiteUser CASCADE;
-DROP TABLE IF EXISTS Bestand CASCADE;
-DROP TABLE IF EXISTS Status CASCADE;
+DROP TRIGGER IF EXISTS trg_check_submission_before_deadline ON submission;
+DROP FUNCTION IF EXISTS check_submission_before_deadline();
 
+DROP TABLE IF EXISTS submission CASCADE;
+DROP TABLE IF EXISTS status CASCADE;
+DROP TABLE IF EXISTS project CASCADE;
+DROP TABLE IF EXISTS student_group CASCADE;
+DROP TABLE IF EXISTS team CASCADE;
+DROP TABLE IF EXISTS student_subject CASCADE;
+DROP TABLE IF EXISTS teacher_subject CASCADE;
+DROP TABLE IF EXISTS subject CASCADE;
+DROP TABLE IF EXISTS website_user CASCADE;
+DROP TABLE IF EXISTS file CASCADE;
 
-CREATE TABLE WebsiteUser(
-    azureObjectId text PRIMARY KEY,
-    is_admin boolean
+CREATE TABLE website_user (
+    uid INT PRIMARY KEY,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE Vak (
-    vak_id TEXT PRIMARY KEY,
-    Naam TEXT NOT NULL
+CREATE TABLE subject (
+    subject_id TEXT PRIMARY KEY,
+    name TEXT NOT NULL
 );
 
-CREATE TABLE StudentVak (
-    azureObjectId TEXT NOT NULL,
-    vak_id TEXT NOT NULL,
-    PRIMARY KEY (azureObjectId, vak_id),
-    FOREIGN KEY (azureObjectId) REFERENCES WebsiteUser(azureObjectId) ON DELETE CASCADE,
-    FOREIGN KEY (vak_id) REFERENCES Vak(vak_id) ON DELETE CASCADE
+CREATE TABLE student_subject (
+    uid INT NOT NULL,
+    subject_id TEXT NOT NULL,
+    PRIMARY KEY (uid, subject_id),
+    FOREIGN KEY (uid) REFERENCES website_user(uid) ON DELETE CASCADE,
+    FOREIGN KEY (subject_id) REFERENCES subject(subject_id) ON DELETE CASCADE
 );
 
-CREATE TABLE LesgeverVak (
-    azureObjectId TEXT NOT NULL,
-    vak_id TEXT NOT NULL,
-    PRIMARY KEY (azureObjectId, vak_id),
-    FOREIGN KEY (azureObjectId) REFERENCES WebsiteUser(azureObjectId) ON DELETE CASCADE,
-    FOREIGN KEY (vak_id) REFERENCES Vak(vak_id) ON DELETE CASCADE
+CREATE TABLE teacher_subject (
+    uid INT NOT NULL,
+    subject_id TEXT NOT NULL,
+    PRIMARY KEY (uid, subject_id),
+    FOREIGN KEY (uid) REFERENCES website_user(uid) ON DELETE CASCADE,
+    FOREIGN KEY (subject_id) REFERENCES subject(subject_id) ON DELETE CASCADE
 );
 
-
-CREATE TABLE Project (
+CREATE TABLE project (
     id BIGSERIAL PRIMARY KEY,
-    deadline DATE,
-    naam TEXT NOT NULL,
-    vak_id TEXT NOT NULL,
-    FOREIGN KEY (vak_id) REFERENCES Vak(vak_id) ON DELETE SET NULL
+    deadline DATE NOT NULL,
+    name TEXT NOT NULL,
+    subject_id TEXT NOT NULL,
+    FOREIGN KEY (subject_id) REFERENCES subject(subject_id) ON DELETE SET NULL
 );
 
-CREATE TABLE Groep (
-    groep_id BIGSERIAL PRIMARY KEY,
-    groep TEXT NOT NULL,
-    score BIGINT,
-    project_id BIGINT,
-    FOREIGN KEY (project_id) REFERENCES Project(id) ON DELETE CASCADE
+CREATE TABLE team (
+    team_id BIGSERIAL PRIMARY KEY,
+    team_name TEXT NOT NULL,
+    score BIGINT NOT NULL,
+    project_id BIGINT NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES project(id) ON DELETE CASCADE,
+    CONSTRAINT score_check CHECK (score BETWEEN 0 AND 20)
 );
 
-
-CREATE TABLE StudentGroep (
-    azureObjectId TEXT NOT NULL,
-    groep_id BIGINT NOT NULL,
-    PRIMARY KEY (azureObjectId, groep_id),
-    FOREIGN KEY (azureObjectId) REFERENCES WebsiteUser(azureObjectId) ON DELETE CASCADE,
-    FOREIGN KEY (groep_id) REFERENCES Groep(groep_id) ON DELETE CASCADE
+CREATE TABLE student_group (
+    uid INT NOT NULL,
+    team_id BIGINT NOT NULL,
+    PRIMARY KEY (uid, team_id),
+    FOREIGN KEY (uid) REFERENCES website_user(uid) ON DELETE CASCADE,
+    FOREIGN KEY (team_id) REFERENCES team(team_id) ON DELETE CASCADE
 );
 
-CREATE TABLE Status (
+CREATE TABLE status (
     status_id BIGSERIAL PRIMARY KEY,
     status_name TEXT NOT NULL UNIQUE
 );
 
-INSERT INTO Status (status_name) VALUES ('inProgress');
-INSERT INTO Status (status_name) VALUES ('Accepted');
-INSERT INTO Status (status_name) VALUES ('Denied');
+INSERT INTO status (status_name) VALUES ('InProgress');
+INSERT INTO status (status_name) VALUES ('Accepted');
+INSERT INTO status (status_name) VALUES ('Denied');
 
-
-CREATE TABLE Indiening (
-    indiening_id BIGSERIAL PRIMARY KEY,
-    datum TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    groep_id BIGINT NOT NULL,
+CREATE TABLE submission (
+    submission_id BIGSERIAL PRIMARY KEY,
+    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    team_id BIGINT NOT NULL,
     project_id BIGINT NOT NULL,
-    status_id BIGINT NOT NULL DEFAULT 1, -- Default to 'inProgress'
-    FOREIGN KEY (groep_id) REFERENCES Groep(groep_id) ON DELETE CASCADE,
-    FOREIGN KEY (project_id) REFERENCES Project(id) ON DELETE CASCADE,
-    FOREIGN KEY (status_id) REFERENCES Status(status_id) ON DELETE RESTRICT
+    status_id BIGINT NOT NULL DEFAULT 1, -- Default to 'InProgress'
+    FOREIGN KEY (team_id) REFERENCES team(team_id) ON DELETE CASCADE,
+    FOREIGN KEY (project_id) REFERENCES project(id) ON DELETE CASCADE,
+    FOREIGN KEY (status_id) REFERENCES status(status_id) ON DELETE RESTRICT
 );
 
-CREATE TABLE Bestand (
-    bestand_id BIGSERIAL PRIMARY KEY,
-    indiening_id BIGINT,  -- mag null zijn -> kunnen linken aan zowel indiening of project of standalone
-    project_id BIGINT,    -- same
-    -- TODO nog effectief bestand columns toevoegen ie file type etc.. not sure about exact implementatie rn
-    FOREIGN KEY (indiening_id) REFERENCES Indiening(indiening_id) ON DELETE SET NULL,
-    FOREIGN KEY (project_id) REFERENCES Project(id) ON DELETE SET NULL
+CREATE TABLE file (
+    file_id BIGSERIAL PRIMARY KEY,
+    submission_id BIGINT,  -- Optional, can be linked to a submission, project, or standalone
+    project_id BIGINT,     -- Optional, same as above
+    FOREIGN KEY (submission_id) REFERENCES submission(submission_id) ON DELETE SET NULL,
+    FOREIGN KEY (project_id) REFERENCES project(id) ON DELETE SET NULL
 );
 
+-- Constraints and checks
+ALTER TABLE project ADD CONSTRAINT deadline_check CHECK (deadline >= CURRENT_DATE);
 
---checks
-
-ALTER TABLE Project
-ADD CONSTRAINT deadline_check CHECK (deadline >= CURRENT_DATE);
-
-ALTER TABLE Groep ADD CONSTRAINT score_check CHECK (score BETWEEN 0 AND 20);
-
-ALTER TABLE WebsiteUser ALTER COLUMN is_admin SET DEFAULT FALSE;
-
---zorgt ervoor dat er geen indieningen na deadline kunnen gebeuren
-CREATE OR REPLACE FUNCTION check_indiening_voor_deadline()
+-- Trigger to prevent submissions after the project deadline
+CREATE OR REPLACE FUNCTION check_submission_before_deadline()
 RETURNS TRIGGER AS $$
 DECLARE
     project_deadline DATE;
 BEGIN
-    SELECT deadline INTO project_deadline
-    FROM Project
-    WHERE id = NEW.project_id;
+    SELECT deadline INTO project_deadline FROM project WHERE id = NEW.project_id;
 
     IF CURRENT_TIMESTAMP > project_deadline THEN
-        RAISE EXCEPTION 'Indiening is niet meer toegelaten na deadline';
+        RAISE EXCEPTION 'Submissions are not allowed after the deadline.';
     END IF;
 
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER trg_check_indiening_voor_deadline
-BEFORE INSERT ON Indiening
+CREATE TRIGGER trg_check_submission_before_deadline
+BEFORE INSERT ON submission
 FOR EACH ROW
-EXECUTE FUNCTION check_indiening_voor_deadline();
-
-
-
-
-
-
-
-
-
-
-
-
+EXECUTE FUNCTION check_submission_before_deadline();
