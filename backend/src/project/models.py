c5bd3b5c18ece6cb83ab262413097531180b5372
drieshuybens <huybensdries@Gmail.com>
2024-03-11 19:11:49 +0100

    alembic added

--- backend/src/project/models.py
+++ backend/src/project/models.py
@@ -1,17 +1,21 @@
-from sqlalchemy import Column, BigInteger, String, Date, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, BigInteger, String, Date, ForeignKey, CheckConstraint
+from sqlalchemy.orm import relationship, Mapped
 from src.database import Base
 
 
 class Project(Base):
     __tablename__ = 'project'
 
-    id = Column(BigInteger, primary_key=True, autoincrement=True, index=True)
-    deadline = Column(Date, nullable=False, check_constraint='deadline >= CURRENT_DATE')
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    deadline = Column(Date, nullable=False)
     name = Column(String, nullable=False)
-    subjectId = Column(String, ForeignKey(
-        'Subject.subjectId', ondelete="SET NULL"), nullable=True)
+    subject_id = Column(BigInteger, ForeignKey('subject.id', ondelete="SET NULL"), nullable=True)  # Adjusted to BigInteger
     description = Column(String, nullable=True)
+    enroll_deadline = Column(Date, nullable=True)
 
     # Relationships
-    teams = relationship("Team", back_populates="project")
+    subject = relationship("Subject")
+
+    __table_args__ = (
+        CheckConstraint('deadline >= CURRENT_DATE', name='deadline_check'),
+    )
