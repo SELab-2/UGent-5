07ab30533efa3c41f6b214c0ff96b5aeb2747aa3
miboelae <michael.boelaert@ugent.be>
2024-03-11 13:40:34 +0100

    again linter :)

--- backend/src/group/exceptions.py
+++ backend/src/group/exceptions.py
@@ -5,4 +5,3 @@ class GroupNotFound(HTTPException):
     def __init__(self):
         """Raised when group is not found in database"""
         super().__init__(status_code=404, detail="Group not found")
-        
