32b288fab1d3207743ed6f0333b006ddb6f4eb3b
Xander Bil <47951455+xerbalind@users.noreply.github.com>
2024-03-13 21:49:47 +0000

    Tests fixes + async (#56)
    * nrml alles naar async
    
    * Fix tests for async
    
    * final
    
    ---------
    
    Co-authored-by: drieshuybens <huybensdries@Gmail.com>

--- backend/src/subject/dependencies.py
+++ backend/src/subject/dependencies.py
@@ -1,6 +1,7 @@
 from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
-from src.dependencies import get_db
+from src.dependencies import get_async_db, get_db
 from src.user.dependencies import get_authenticated_user
 from src.auth.exceptions import NotAuthorized
 from src.user.schemas import User
@@ -10,7 +11,7 @@ from .exceptions import SubjectNotFound
 from .schemas import Subject, SubjectList
 
 
-async def retrieve_subject(subject_id: int, db: Session = Depends(get_db)) -> Subject:
+async def retrieve_subject(subject_id: int, db: AsyncSession = Depends(get_async_db)) -> Subject:
     subject = await service.get_subject(db, subject_id)
     if not subject:
         raise SubjectNotFound()
@@ -19,7 +20,7 @@ async def retrieve_subject(subject_id: int, db: Session = Depends(get_db)) -> Su
 
 
 async def retrieve_subjects(
-    user: User = Depends(get_authenticated_user), db: Session = Depends(get_db)
+    user: User = Depends(get_authenticated_user), db: AsyncSession = Depends(get_async_db)
 ) -> SubjectList:
     teacher_subjects, student_subjects = await service.get_subjects(db, user.uid)
     return SubjectList(as_teacher=teacher_subjects, as_student=student_subjects)
@@ -28,7 +29,7 @@ async def retrieve_subjects(
 async def user_permission_validation(
     subject_id: int,
     user: User = Depends(get_authenticated_user),
-    db: Session = Depends(get_db),
+    db: AsyncSession = Depends(get_async_db),
 ):
     if not user.is_admin:
         teachers = await service.get_teachers(db, subject_id)
