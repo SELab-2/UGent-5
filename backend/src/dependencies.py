40b5b556370043b4f44cca85ad1c1efb6e14ebf0
Bram Reyniers <55666730+reyniersbram@users.noreply.github.com>
2024-03-13 23:34:30 +0100

    cleanup old non-async database code

--- backend/src/dependencies.py
+++ backend/src/dependencies.py
@@ -1,13 +1,4 @@
-from .database import SessionLocal, AsyncSessionLocal
-
-
-def get_db():
-    """Creates new database session per request, which is closed afterwards"""
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+from .database import AsyncSessionLocal
 
 
 async def get_async_db():
