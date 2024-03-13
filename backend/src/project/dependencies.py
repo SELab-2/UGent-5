b0015e3398370391e91542bf67403219e9ae2b07
Bram Reyniers <55666730+reyniersbram@users.noreply.github.com>
2024-03-13 19:02:51 +0100

    some fixes and formatting

--- backend/src/project/dependencies.py
+++ backend/src/project/dependencies.py
@@ -1,6 +1,6 @@
 from fastapi import Depends
-from sqlalchemy.orm import Session
-from src.dependencies import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.dependencies import get_async_db
 from src.user.dependencies import get_authenticated_user
 from src.user.schemas import User
 
@@ -11,7 +11,7 @@ from ..subject.service import get_teachers
 async def user_permission_validation(
     subject_id: int,
     user: User = Depends(get_authenticated_user),
-    db: Session = Depends(get_db),
+    db: AsyncSession = Depends(get_async_db),
 ):
     if not user.is_admin:
         teachers = await get_teachers(db, subject_id)
--- backend/src/subject/dependencies.py
+++ backend/src/subject/dependencies.py
@@ -1,9 +1,8 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Session
-from src.dependencies import get_async_db, get_db
-from src.user.dependencies import get_authenticated_user
 from src.auth.exceptions import NotAuthorized
+from src.dependencies import get_async_db
+from src.user.dependencies import get_authenticated_user
 from src.user.schemas import User
 
 from . import service
@@ -11,16 +10,19 @@ from .exceptions import SubjectNotFound
 from .schemas import Subject, SubjectList
 
 
-async def retrieve_subject(subject_id: int, db: AsyncSession = Depends(get_async_db)) -> Subject:
+async def retrieve_subject(
+    subject_id: int, db: AsyncSession = Depends(get_async_db)
+) -> Subject:
     subject = await service.get_subject(db, subject_id)
     if not subject:
         raise SubjectNotFound()
 
-    return Subject.model_validate(subject)
+    return subject
 
 
 async def retrieve_subjects(
-    user: User = Depends(get_authenticated_user), db: AsyncSession = Depends(get_async_db)
+    user: User = Depends(get_authenticated_user),
+    db: AsyncSession = Depends(get_async_db),
 ) -> SubjectList:
     teacher_subjects, student_subjects = await service.get_subjects(db, user.uid)
     return SubjectList(as_teacher=teacher_subjects, as_student=student_subjects)
