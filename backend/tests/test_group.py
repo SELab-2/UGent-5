bdc5a753a68c4557d74e059c374ce7f0e5bfedb5
miboelae <michael.boelaert@ugent.be>
2024-03-14 19:21:29 +0100

    :)

--- backend/tests/test_group.py
+++ backend/tests/test_group.py
@@ -29,11 +29,21 @@ async def project_id(client: AsyncClient, db: AsyncSession, subject_id: int):
         "deadline": future_date.strftime("%Y-%m-%dT%H:%M:%SZ"),
         "description": "test",
     }
-    await set_admin(db, "test", True)
+    await set_admin(db, "test", True) 
     response = await client.post("/api/projects/", json=project)
-    print(response.status_code)
-    print(response.json())
     return response.json()["id"]
+
+
+@pytest_asyncio.fixture
+async def group_id(client: AsyncClient, db: AsyncSession, subject_id: int):
+    group_data = {
+        'team_name': 'test group',
+        'project_id': project_id
+    }
+    await set_admin(db, "test", True) 
+    response = await client.post("/api/groups/", json=group_data)
+    return response.json()["id"]
+
 
 @pytest.mark.asyncio
 async def test_create_group(client: AsyncClient, db: AsyncSession, project_id: int) -> int:
@@ -42,11 +52,9 @@ async def test_create_group(client: AsyncClient, db: AsyncSession, project_id: i
         'project_id': project_id
     }
     await set_admin(db, "test", True)
-    response = await client.post(f"/api/projects/{project_id}/groups/", json=group_data)
-    
-    print(response.json())
-    print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-    return response.json()["id"]
+    response = await client.post(f"/api/groups/", json=group_data)
+    assert response.status_code == 201  # Created
+    assert response.json()["team_name"] == group_data["team_name"]
 
 # group_data = {
 #          'team_name': 'test group',
@@ -55,7 +63,7 @@ async def test_create_group(client: AsyncClient, db: AsyncSession, project_id: i
 
 @pytest.mark.asyncio
 async def test_get_group(client: AsyncClient, group_id: int):
-    response = await client.get(f"/api/projects/{project_id}/groups/{group_id}")
+    response = await client.get(f"/api/groups/{group_id}")
     assert response.status_code == 200
     assert response.json()["id"] == group_id
 
@@ -63,18 +71,18 @@ async def test_get_group(client: AsyncClient, group_id: int):
 @pytest.mark.asyncio
 async def test_remove_user(client: AsyncClient, db: AsyncSession, group_id: int):
     await set_admin(db, "test", False)
-    response = await client.delete(f"/api/projects/{project_id}/groups/{group_id}")
+    response = await client.delete(f"/api/groups/{group_id}")
     assert response.status_code == 403  # Forbidden
     await set_admin(db, "test", True)
-    response = await client.delete(f"/api/projects/{project_id}/groups/{group_id}")
+    response = await client.delete(f"/api/groups/{group_id}")
     assert response.status_code == 200
 
 
 @pytest.mark.asyncio
 async def test_join_user(client: AsyncClient, db: AsyncSession, group_id: int):
     await set_admin(db, "test", False)
-    response = await client.post(f"/api/projects/{project_id}/groups/{group_id}")
+    response = await client.post(f"/api/groups/{group_id}")
     assert response.status_code == 403  # Forbidden
     await set_admin(db, "test", True)
-    response = await client.post(f"/api/projects/{project_id}/groups/{group_id}")
+    response = await client.post(f"/api/groups/{group_id}")
     assert response.status_code == 200
