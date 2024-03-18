0afd070136c90d64e8243faa35eac1605d703df8
Marieke <marieke.sinnaeve@ugent.be>
2024-03-18 20:30:29 +0100

    vuetify

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,16 @@
 <template>
-    <ApolloHeader v-if="!hideHeader" />
-    <RouterView />
+    <v-app>
+        <v-main>
+            <ApolloHeader v-if="!hideHeader" />
+            <router-view />
+        </v-main>
+    </v-app>
 </template>
 
 <script setup lang="ts">
-import { RouterView, useRouter } from "vue-router";
+import { defineComponent, computed } from "vue";
+import { useRouter } from "vue-router";
 import ApolloHeader from "@/components/ApolloHeader.vue";
-import { computed } from "vue";
 const router = useRouter();
 const hideHeader = computed(() => router.currentRoute.value.meta.hideHeader === true);
 </script>
-
-<style scoped></style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,13 +2,13 @@ import "./assets/main.scss";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
 import App from "./App.vue";
 import router from "./router";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
 
-const app = createApp(App);
+const vuetify = createVuetify({
+    components,
+});
 
-app.use(createPinia());
-app.use(router);
-
-app.mount("#app");
+createApp(App).use(router).use(createPinia()).use(vuetify).mount("#app");
