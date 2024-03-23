c24891c87bf766fb92eed77db367f7aa0a3d886a
Pieter Janin <Pieter.Janin@UGent.be>
2024-03-23 15:15:12 +0100

    wip

---
+++ backend/src/docker_tests/docker_default/default_image.dockerfile
@@ -0,0 +1,27 @@
+FROM python:3
+
+WORKDIR /home/runner
+
+COPY checks/* /home/runner/
+COPY entrypoint /home/runner/
+
+RUN useradd -m runner
+RUN chown -R runner:runner /home/runner/
+
+# C u
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+        # cmd line utils
+        coreutils \
+        curl \
+		wget \
+        # C utils
+        gcc \
+        g++ \
+        make
+
+
+# Python packages
+RUN pip install --no-cache-dir --upgrade numpy pandas
+
+ENTRYPOINT ["/home/runner/entrypoint"]
--- backend/src/docker_tests/service.py
+++ backend/src/docker_tests/service.py
@@ -3,17 +3,22 @@ import docker
 async def build_image():
     client = docker.from_env()
 
-    client.images.build(path="/Users/pieterjanin/UGent-5/backend/src/docker_tests/docker_base", tag="docker_base", rm=True)
-    client.images.build(path="/Users/pieterjanin/UGent-5/backend/src/docker_tests/docker_example", tag="docker_example", rm=True)
+    client.images.build(
+        path='/Users/pieterjanin/UGent-5/backend/src/docker_tests/docker_default/', 
+        dockerfile="/Users/pieterjanin/UGent-5/backend/src/docker_tests/docker_default/default_image.dockerfile",
+        tag="default_image",
+        forcerm=True
+    )
     client.images.prune()
 
 
 async def run_docker_tests_detatched():
     client = docker.from_env()
     container = client.containers.run(
-        image='docker_example',
+        image='default_image',
         volumes={
-            '/Users/pieterjanin/docker_example_bash/submission2/': {'bind': '/submission', 'mode': 'ro'},
+            '/Users/pieterjanin/UGent-5/backend/src/docker_tests/docker_default/checks': {'bind': '/checks', 'mode': 'ro'},
+            '/Users/pieterjanin/docker_example_bash/submission1': {'bind': '/submission', 'mode': 'ro'},
             '/Users/pieterjanin/docker_example_bash/out': {'bind': '/artifacts', 'mode': 'rw'}
         },
         detach=True,
@@ -27,7 +32,7 @@ async def run_docker_tests_detatched():
 
 async def run_docker_tests():
     client = docker.from_env()
-    output = client.containers.run(image='docker_example', remove=True, stream=True)
+    output = client.containers.run(image='default_image', remove=True, stream=True)
 
     for log in output:
         print(log.decode('UTF-8'), end='')
