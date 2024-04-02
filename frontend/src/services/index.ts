43c895003f52fc05cbc029d629e40d095e83c77b
Bram Reyniers <reyniersbram@gmail.com>
2024-04-02 21:19:05 +0200

    improve authorized_fetch

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,28 +1,16 @@
 import { useAuthStore } from "@/stores/auth-store";
 
-/**
- * Request options for the fetch function
- * @param method: HTTP method
- * @param headers: Request headers
- * @param body: Request body
- */
-export interface RequestOptions {
-    method: "GET" | "POST" | "PATCH" | "DELETE";
-    headers?: Object;
-    body?: string;
-}
-
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 /**
  * Fetch data from the API
- * @param endpoint: API endpoint
- * @param requestOptions: Custom request options
+ * @param endpoint API endpoint
+ * @param requestOptions Custom request options
  * @returns Response from the API
  */
 export async function authorized_fetch<T>(
     endpoint: string,
-    requestOptions: RequestOptions
+    requestOptions: RequestInit
 ): Promise<T> {
     const { token } = useAuthStore();
     const response = await fetch(`${BASE_URL}${endpoint}`, {
@@ -30,6 +18,7 @@ export async function authorized_fetch<T>(
         headers: {
             Authorization: `${token?.token_type} ${token?.token}`,
             "Content-Type": "application/json",
+            ...requestOptions.headers,
         },
     });
     if (!response.ok) {
