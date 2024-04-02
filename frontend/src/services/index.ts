a286bce6fc4ae5aa619eb161f7f71269ae03454f
Bram Reyniers <reyniersbram@gmail.com>
2024-04-03 01:38:48 +0200

    fix authorized_fetch

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -6,20 +6,28 @@ const BASE_URL = import.meta.env.VITE_API_URL;
  * Fetch data from the API
  * @param endpoint API endpoint
  * @param requestOptions Custom request options
+ * @param omitContentType Omit the Content-Type header
  * @returns Response from the API
  */
 export async function authorized_fetch<T>(
     endpoint: string,
-    requestOptions: RequestInit
+    requestOptions: RequestInit,
+    omitContentType: boolean = false
 ): Promise<T> {
     const { token } = useAuthStore();
+    const { "Content-Type": contentType, ...strippedHeaders } = {
+        Authorization: `${token?.token_type} ${token?.token}`,
+        "Content-Type": "application/json",
+        ...requestOptions.headers,
+    };
+
+    const headers = omitContentType
+        ? strippedHeaders
+        : { ...strippedHeaders, "Content-Type": contentType };
+
     const response = await fetch(`${BASE_URL}${endpoint}`, {
         ...requestOptions,
-        headers: {
-            Authorization: `${token?.token_type} ${token?.token}`,
-            "Content-Type": "application/json",
-            ...requestOptions.headers,
-        },
+        headers: headers,
     });
     if (!response.ok) {
         throw new Error(response.statusText);
