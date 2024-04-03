9f8fe1438437e9ceec01f4a9a9c24722809cf294
Bram Reyniers <reyniersbram@gmail.com>
2024-04-03 12:44:10 +0200

    migrate to vue-query

--- frontend/src/views/student/HomeScreenStudentView.vue
+++ frontend/src/views/student/HomeScreenStudentView.vue
@@ -1,6 +1,7 @@
 <template>
     <BackgroundContainer>
-        <h1>{{ $t("home.welcome", { name: user }) }}</h1>
+        <h1 v-if="isLoading">Loading...</h1>
+        <h1 v-else>{{ $t("home.welcome", { name: user!.given_name }) }}</h1>
         <v-container>
             <v-row>
                 <v-col>
@@ -18,31 +19,11 @@
 </template>
 
 <script setup lang="ts">
-import { useAuthStore } from "@/stores/auth-store";
-import { ref, onMounted } from "vue";
 import HomeScreenCard from "@/components/cards/HomeScreenCard.vue";
 import BackgroundContainer from "@/components/BackgroundContainer.vue";
+import { useUserQuery } from "@/queries/User";
 
-const apiUrl = import.meta.env.VITE_API_URL;
-const user = ref<string | null>(null);
-const { token } = useAuthStore();
-onMounted(async () => {
-    await fetchUser();
-});
-
-async function fetchUser() {
-    if (!token) {
-        return;
-    }
-    await fetch(`${apiUrl}/api/users/me`, {
-        headers: { Authorization: `${token?.token_type} ${token?.token}` },
-    })
-        .then((data) => data.json())
-        .then((userObj) => {
-            user.value = userObj.given_name;
-        })
-        .catch((error) => console.log(error));
-}
+const { data: user, isLoading } = useUserQuery();
 </script>
 
 <style scoped lang="scss">
