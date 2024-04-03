e6dcaa25fe806a21d4394b6bf8b1bb12b89c3f34
Bram Reyniers <reyniersbram@gmail.com>
2024-04-03 21:24:44 +0200

    routes to placeholder pages

--- frontend/src/composables/useNavigations.ts
+++ frontend/src/composables/useNavigations.ts
@@ -9,13 +9,13 @@ interface NavigationData {
 
 const main_navigations: NavigationData[] = [
     { icon: "mdi-home-outline", title: "navigation.home", goto: "home" },
-    { icon: "mdi-school-outline", title: "navigation.courses", goto: "about" },
-    { icon: "mdi-book-check-outline", title: "navigation.projects", goto: "about" },
-    { icon: "mdi-cog-outline", title: "navigation.settings", goto: "about" },
+    { icon: "mdi-school-outline", title: "navigation.courses", goto: "courses" },
+    { icon: "mdi-book-check-outline", title: "navigation.projects", goto: "projects" },
+    { icon: "mdi-cog-outline", title: "navigation.settings", goto: "settings" },
 ];
 
 const admin_navigations: NavigationData[] = [
-    { icon: "mdi-security", title: "navigation.admin", goto: "about" },
+    { icon: "mdi-security", title: "navigation.admin", goto: "admin" },
 ];
 
 export default function useNavigations() {
