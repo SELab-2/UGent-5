51fbdcb57a3b1893c03df79fec27c8e98a93a19f
drieshuybens <huybensdries@Gmail.com>
2024-04-04 21:43:07 +0200

    extra needed fields for creation

--- backend/src/project/models.py
+++ backend/src/project/models.py
@@ -4,18 +4,20 @@ from sqlalchemy import CheckConstraint, DateTime, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.database import Base
 from typing import List
-
-
 class Project(Base):
     __tablename__ = "project"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     deadline: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), nullable=False)
+    publish_date: Mapped[datetime] = mapped_column(
+            DateTime(timezone=True), nullable=False)
     name: Mapped[str] = mapped_column(nullable=False)
+    group_selection: Mapped[str] = mapped_column(nullable=True)
     subject_id: Mapped[int] = mapped_column(
         ForeignKey("subject.id", ondelete="CASCADE"), nullable=True
     )
+    involved_instructors: Mapped[List[str]] = mapped_column(ARRAY(String))
     description: Mapped[str] = mapped_column(nullable=True)
     enroll_deadline: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), nullable=True
