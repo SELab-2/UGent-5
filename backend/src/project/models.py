000a925f7c0eb9e7caa424e4fc41d4fcdaec95f8
drieshuybens <huybensdries@Gmail.com>
2024-04-04 21:50:05 +0200

    import

--- backend/src/project/models.py
+++ backend/src/project/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import CheckConstraint, DateTime, ForeignKey
+from sqlalchemy import CheckConstraint, DateTime, ForeignKey, ARRAY, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.database import Base
 from typing import List
