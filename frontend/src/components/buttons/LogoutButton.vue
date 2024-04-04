425c4b97da7968825c66536f9767d1519f49eabf
Marieke <marieke.sinnaeve@ugent.be>
2024-04-04 15:16:27 +0200

    mobile header + changed homescreens

---
+++ frontend/src/components/buttons/LogoutButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-btn variant="text" class="logout" @click="logout"> logout </v-btn>
+</template>
+
+<script setup lang="ts">
+import {useAuthStore} from "@/stores/auth-store";
+
+const { logout } = useAuthStore();
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/navigation/DropDownMobile.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-menu transition="slide-y-transition">
+        <template v-slot:activator="{ props }">
+            <v-btn v-bind="props" class="menu">
+                <icon class="mdi mdi-dots-vertical"></icon>
+            </v-btn>
+        </template>
+        <v-list class="items">
+            <v-list-item>
+                <LogoutButton />
+            </v-list-item>
+            <v-list-item>
+                <LocaleSwitcher />
+            </v-list-item>
+        </v-list>
+    </v-menu>
+</template>
+
+<script setup lang="ts">
+import LocaleSwitcher from "@/components/LocaleSwitcher.vue"
+import LogoutButton from "@/components/buttons/LogoutButton.vue"
+
+</script>
+
+<style scoped>
+.menu {
+    margin-right: 10px;
+}
+
+.items {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
