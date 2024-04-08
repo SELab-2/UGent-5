489d0353eae8d1d4ed42c24c9c4c518a9d840788
Bram Reyniers <reyniersbram@gmail.com>
2024-04-08 16:39:04 +0200

    use testPinia

--- frontend/vitest.setup.ts
+++ frontend/vitest.setup.ts
@@ -2,5 +2,6 @@ import { config } from '@vue/test-utils'
 import i18n from '@/i18n/index.ts'
 import vuetify from '@/plugins/vuetify.ts'
 import {VueQueryPlugin} from "@tanstack/vue-query";
+import { createTestingPinia } from "@pinia/testing";
 
-config.global.plugins = [i18n, vuetify, VueQueryPlugin]
+config.global.plugins = [i18n, vuetify, VueQueryPlugin, createTestingPinia()]
