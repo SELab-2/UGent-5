1f6150728d9482db506796e730a0467b2eb9c42d
Marieke <marieke.sinnaeve@ugent.be>
2024-04-13 15:45:24 +0200

    fixed navbar mock data

--- frontend/tests/components/navigation/NavBar.spec.ts
+++ frontend/tests/components/navigation/NavBar.spec.ts
@@ -1,23 +1,46 @@
 import { mount } from '@vue/test-utils'
-import { h } from "vue";
+import {computed, h, ref} from "vue";
 import { VLayout } from "vuetify/components";
 import { expect, describe, it, vi} from 'vitest'
 import NavBar from "@/components/navigation/NavBar.vue"
-import {useToggleAdminMutation, useUserQuery} from "../../../src/queries/User";
+
+const mockRouter = {
+    push: vi.fn()
+};
+
+vi.mock('@/composables/useIsAdmin')
+vi.mock('@/stores/auth-store')
+
+// Mock useRouter to return the mock router
+vi.mock('vue-router', () => ({
+    useRouter: () => mockRouter
+}));
 
 describe('NavBar', () => {
+    vi.mock('@/stores/auth-store', () => ({
+        __esModule: true,
+        useAuthStore: () => ({
+            isLoggedIn: ref(true),
+        }),
+    }));
+
     const ResizeObserverMock = vi.fn(() => ({
         observe: vi.fn(),
         unobserve: vi.fn(),
         disconnect: vi.fn(),
     }));
+
     vi.stubGlobal('ResizeObserver', ResizeObserverMock);
-    const wrapper = mount(VLayout, {
-        slots: {
-            default: h(NavBar)
-        }
-    })
-    it('renders the correct main navigations', () => {
+
+    it('renders correct navigations for non admin', async () => {
+        const adminStore = await import('@/composables/useIsAdmin')
+        adminStore.default = () => ({ isAdmin: computed(() => false) });
+
+        const wrapper = mount(VLayout, {
+            slots: {
+                default: h(NavBar)
+            }
+        })
 
         expect(wrapper.find('[data-test="navigation.home"]').exists()).toBeTruthy();
         expect(wrapper.find('[data-test="navigation.courses"]').exists()).toBeTruthy();
@@ -28,7 +51,17 @@ describe('NavBar', () => {
     });
 
     it('render correct navigations for admin', async () => {
-
-        //expect(wrapper.find('[data-test="navigation.admin"]').exists()).toBeTruthy();
+        const adminStore = await import('@/composables/useIsAdmin')
+        adminStore.default = () => ({ isAdmin: computed(() => true) });
+        const wrapper = mount(VLayout, {
+            slots: {
+                default: h(NavBar)
+            }
+        })
+        expect(wrapper.find('[data-test="navigation.home"]').exists()).toBeTruthy();
+        expect(wrapper.find('[data-test="navigation.courses"]').exists()).toBeTruthy();
+        expect(wrapper.find('[data-test="navigation.projects"]').exists()).toBeTruthy();
+        expect(wrapper.find('[data-test="navigation.settings"]').exists()).toBeTruthy();
+        expect(wrapper.find('[data-test="navigation.admin"]').exists()).toBeTruthy();
     })
 })
