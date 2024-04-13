1d4f2dd5d13d6cc688380153815c3a68d1efbed9
miboelae <michael.boelaert@ugent.be>
2024-04-13 10:37:04 +0200

    add helpbutton and other changes

--- frontend/src/components/project/ProjectInfo.vue
+++ frontend/src/components/project/ProjectInfo.vue
@@ -1,15 +1,35 @@
 <template>
-    <div>
-        <h1>{{ project.name }}</h1>
-    </div>
+    <BackgroundContainer>
+        <v-col class="scrollable-container">
+            <ProjectInfoCard :project="project" class="scrollable-container"></ProjectInfoCard>
+        </v-col>
+    </BackgroundContainer>
 </template>
 
 <script setup lang="ts">
 import type Project from "@/models/Project";
+import BackgroundContainer from "@/components/BackgroundContainer.vue";
+import ProjectInfoCard from "@/components/project/ProjectInfoCard.vue";
+
 const props = defineProps<{
     project: Project;
 }>();
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.background-container {
+    padding-bottom: 20px; /* Add padding to the bottom */
+}
+
+.scrollable-container {
+    max-height: calc(100% - 20px);
+    overflow-y: auto;
+}
+
+.scrollable-container .content {
+    white-space: nowrap; /* Prevent text from wrapping */
+    overflow: hidden; /* Hide overflowing content */
+    text-overflow: ellipsis; /* Display ellipsis (...) for overflowed text */
+}
+</style>
---
+++ frontend/src/components/project/ProjectInfoCard.vue
@@ -0,0 +1,211 @@
+<template>
+    <v-card color="white">
+        <v-card-title>{{project?.name}}</v-card-title>
+        <v-container>
+            <v-row>
+                {{project?.deadline}}
+                {{subject?.name}}
+            </v-row>
+        </v-container>
+        <v-card-item>
+            <v-card-title>
+                assignment:
+            </v-card-title>
+            <v-card-text>
+
+
+                Marieke — 03/04/2024 20:21
+                i guess da die me title enzo een andere component is
+                dus daje der twee soorten gaat hebbe
+                Micky — 03/04/2024 20:38
+                things i can do while pieter says one sentence:
+                go to the store
+                go back home
+                cook dinner
+                clean the dishes
+                Marieke — 03/04/2024 20:39
+                afstuderen
+                Micky — 03/04/2024 20:39
+                ^^
+                Marieke — 03/04/2024 20:40
+                ik hou legit mijn hart vast voor als die de eindpresentatie moet doen
+                Micky — 03/04/2024 20:41
+                ainaa
+                2 uur later
+                Marieke — 03/04/2024 20:41
+                khoop da die donnies hem het leren bij die workshop
+                Micky — 03/04/2024 21:15
+                khad gezet who was in paris
+                ineenkeer zie ik xander oppoppen
+                oops
+                Marieke — 03/04/2024 21:15
+                kijk ja ge hebt nie gezegd wie eh
+                Marieke — 04/04/2024 16:11
+                Image
+                Micky — 04/04/2024 16:40
+                Ly2 teamlead!!
+                Micky — Today at 10:32
+                yo maronkel
+                Marieke — Today at 11:33
+                Michaël
+                Micky — Today at 11:35
+                problemen lossen zichzelf op met de tijd
+                dont need you anymore
+                maar een heel goedemorgen toegewenst aan mijn teamlead!
+                Marieke — Today at 12:56
+                Die programmeerervaring heb ik dan toch nie precies
+                Micky — Today at 12:59
+                kheb u effe nodig voor die navbar
+                Marieke — Today at 13:00
+                Gij moet toch nie aan de navbar werken?
+                Micky — Today at 13:00
+                nee ma das een bug
+                en kwil die kinda fixed hebben haha
+                Marieke — Today at 13:00
+                Da van in frontend
+                Micky — Today at 13:00
+                ja
+                Marieke — Today at 13:00
+                Ma das gwn omda ge normaal naar /home gaat en da redirevt u naar login als ge nie zijt ingelogd
+                Bij mij is da geen issue dus kwenie
+                Mss is die login check gwn te traag
+                Micky — Today at 13:01
+                ma tis ook als ge een page refresht komt die sidebar er altijd op en dan instant weer weg
+                Marieke — Today at 13:01
+                Das omda die in App.vue normaal altijd er is
+                Gwn bij de login is er zo een router.meta waarde die die uit zet
+                Micky — Today at 13:01
+                zou ge nie een var kunne bijhouden in de app die kijkt of hij openstond voor het refreshen en dan zien of ge hem displayt ofni
+                Micky — Today at 13:02
+                ja ma tis bij elke route da die bar erop komt
+                Marieke — Today at 13:02
+                Das de bedoeling da die open staat bij elke router
+                Alleh da is zo geprogrammeerd nu
+                Kheb nog niks gevonden om die navbar in te clicken als ge naar andere pagina gaat
+                Dus als ge van home ergens anders naartoe gazt is da normaal da die openstaat
+                Micky — Today at 13:04
+                het ding is da em nu bij het refreshen van een pagina gwn effe open komt ma nie openblijft
+                Marieke — Today at 13:04
+                💀
+                Ma uw inspect staat open
+                Micky — Today at 13:04
+                ja
+                ah
+                ...
+                Marieke — Today at 13:04
+                Doe die is dicht en refrsh nog es
+                Micky — Today at 13:04
+                :x
+                Marieke — Today at 13:04
+                Da is zo gemaakt zoda vanaf een bepaalde grootte die inklapt omda anders de cards der nie oppassen 💀💀💀
+                Micky — Today at 15:37
+                abc plan ik 6 dagen voor in de examens, das genoeg e van om van 0 te beginnen?
+                Marieke — Today at 15:37
+                ik zou zeggen van wel
+                Micky — Today at 15:37
+                ik vertrouw op u haha
+                Marieke — Today at 15:37
+                big mistake
+                ma da vak lag mij wel
+                en ik wist wel al iets ervan
+                alleh ik ben naar paar lessen gegaan
+                en ik heb het op 4 dagen een 10 gehaald
+                gewoon weten wa waar in de boek staat en dan oud examens kunne
+                nie de mistake maken van de theorie TE geod te leren i suppose want ja das gwn ctrl f en hopen dajet vind
+                Micky — Today at 15:38
+                ait bet
+                thx
+                ja kga 6 dagen gwn tryharden
+                is mijn eerste vak
+                moet nog wel lukken
+                Micky — Today at 16:44
+                Image
+                die foto
+                mening?
+                like
+                niemand van de proffen wilt een foto daarin steken
+                Marieke — Today at 16:44
+                ja skip gwn de foto
+                uitleg is belangrijker
+                gewoon dikke strook me de uitleg en dan daaronder eventueel die downloadable files en submission dinge naast elkaar
+                ma onder mekaar ook goe
+                moet eerst gwn werken
+                Micky — Today at 16:45
+                also kheb nu zo kolommetjes gemaakt
+                Image
+                Marieke — Today at 16:45
+                de rest is voor milestone 3
+                Micky — Today at 16:45
+                ma hoe zorg ik da het rechtse
+                er goed uitziet
+                maak ik da ook nen backgroundcontainer ding
+                Marieke — Today at 16:45
+                de wondere wereld van css
+                Micky — Today at 16:45
+                okidoki
+                🙂
+                Marieke — Today at 16:45
+                daar moeten components voor gemaakt wroden
+                ge bedoelt toch die ask question ding enzo eh?
+                Micky — Today at 16:46
+                ja
+                is da elk ne component ook nog? 🙂
+                Marieke — Today at 16:46
+                ja iemand gaat daar keer component voor moete maken dan
+                en in de component buttons map zetten
+                ma fix maar eerst uw pagina
+                desnoods hou ik mij daar nog wel mee bezig me die components wa te regelen
+                Micky — Today at 16:47
+                kben flink aant werken e!
+                wel kkr traag maja
+                Marieke — Today at 16:47
+                kijk ja meer uren dan eh
+                tmoet gwn af zijn voor milestone
+                ik ben aant strijden me deze fuckers
+                Image
+                frontend testen 💀
+                Micky — Today at 17:32
+                moet alles echt retard proof zijn of?
+                kheb nu een projectpage ma like ik heb het subject ook nodig om te displayen
+                moe ik effectief checken of het een geldig subjectid is als ik da opzoek via het project
+                in principe kan er geen project zijn zonder subject in de ideale wereld
+                Marieke — Today at 17:33
+                als in via de link?
+                Micky — Today at 17:33
+                als in via de databank
+                Marieke — Today at 17:33
+                mss keer op de vergadering vragen
+                wa de rest zegt
+                ma kvrees van wel
+                Micky — Today at 17:34
+                zoveel werk
+                💀
+                Marieke — Today at 17:34
+                allewel kwenie
+                ma laat het gewoon voorwa het is en vraag het en argumenteer waarom ge da nie wilt doen
+            </v-card-text>
+        </v-card-item>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+import type Project from "@/models/Project";
+import {useSubjectQuery} from "@/queries/Subject";
+import {computed} from "vue";
+import { toRefs } from "vue";
+
+const props = defineProps<{
+    project: Project;
+}>();
+
+const { project } = toRefs(props);
+
+const { data: subject, isLoading: isSubjectLoading } = useSubjectQuery(
+    computed(() => project.value?.subject_id)
+);
+
+</script>
+
+<style scoped>
+
+</style>
