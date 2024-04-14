bf35bf3423b7d3cdea9d076e36f1e0facace6504
Marieke <marieke.sinnaeve@ugent.be>
2024-04-14 16:02:58 +0200

    dropdownlist test

---
+++ frontend/src/components/navigation/DropDownList.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-list class="items">
+        <v-list-item data-test="logoutButton" v-if="loggedInValue.value">
+            <LogoutButton />
+        </v-list-item>
+        <v-list-item data-test="localeSwitcher">
+            <LocaleSwitcher />
+        </v-list-item>
+    </v-list>
+</template>
+
+<script setup lang="ts">
+import LocaleSwitcher from "@/components/LocaleSwitcher.vue";
+import LogoutButton from "@/components/buttons/LogoutButton.vue";
+
+import { useAuthStore } from "@/stores/auth-store";
+import { storeToRefs } from "pinia";
+import {ref} from "vue";
+
+const loggedInValue = ref(false)
+
+const beforeCreate = async function beforeCreate(){
+    const { isLoggedIn } = storeToRefs(useAuthStore());
+    loggedInValue.value = isLoggedIn
+}
+
+beforeCreate()
+
+</script>
+
+<style scoped>
+
+.items {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
