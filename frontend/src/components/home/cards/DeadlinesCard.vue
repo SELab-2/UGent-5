06b772e092ae9edc46528586af4044140e15ebdf
Bram Reyniers <reyniersbram@gmail.com>
2024-04-18 01:03:13 +0200

    big refactor

---
+++ frontend/src/components/home/cards/DeadlinesCard.vue
@@ -0,0 +1,45 @@
+<template>
+    <HomeScreenCard :title="'homescreen.deadlines'">
+        <HomeScreenDeadline v-for="deadline in deadlines" :deadline="deadline" :key="deadline.project.id" />
+    </HomeScreenCard>
+</template>
+
+<script setup lang="ts">
+import HomeScreenCard from "@/components/home/cards/HomeScreenCard.vue";
+import HomeScreenDeadline from "@/components/home/listcontent/HomeScreenDeadline.vue";
+import { type Deadline } from "@/models/Project";
+import { ref } from "vue";
+const deadlines = ref<Deadline[]>([
+    {
+        project: {
+            id: 1,
+            name: "project A",
+            subject_id: 1,
+            deadline: new Date(2024, 4, 28, 23, 59),
+            description: "a description",
+        },
+        status: "none",
+    },
+    {
+        project: {
+            id: 2,
+            name: "project B",
+            subject_id: 2,
+            deadline: new Date(2024, 6, 2, 17, 0),
+            description: "another description",
+        },
+        status: "accepted",
+    },
+    {
+        project: {
+            id: 3,
+            name: "project C",
+            subject_id: 3,
+            deadline: new Date(2024, 5, 5, 22, 0),
+            description: "last description",
+        },
+        status: "rejected",
+    },
+]);
+
+</script>
--- frontend/src/views/HomeScreenView.vue
+++ frontend/src/views/HomeScreenView.vue
@@ -4,23 +4,10 @@
         <h1 v-else>{{ $t("home.welcome", { name: user!.given_name }) }}</h1>
         <v-container v-if="smAndDown">
             <div class="mobileCard">
-                <HomeScreenCard :title="'homescreen.deadlines'">
-                    <HomeScreenDeadlines
-                        v-for="deadline in deadlines"
-                        :deadline="deadline"
-                        :key="deadline.project.id"
-                    />
-                </HomeScreenCard>
+                <DeadlinesCard />
             </div>
             <div class="mobileCard">
-                <HomeScreenSkeletonCard v-if="isSubjectsLoading"/>
-                <HomeScreenCard v-else :title="'homescreen.subjects'">
-                    <HomeScreenCourses
-                        v-for="subject in subjects"
-                        :subject="subject"
-                        :key="subject.id"
-                    />
-                </HomeScreenCard>
+                <SubjectsCard />
             </div>
             <div class="mobileCard">
                 <HomeScreenCard :title="'homescreen.announcements'" />
@@ -29,22 +16,10 @@
         <v-container v-else>
             <v-row>
                 <v-col>
-                    <HomeScreenCard :title="'homescreen.deadlines'">
-                        <HomeScreenDeadlines
-                            v-for="deadline in deadlines"
-                            :deadline="deadline"
-                            :key="deadline.project.id"
-                        />
-                    </HomeScreenCard>
+                    <DeadlinesCard />
                 </v-col>
                 <v-col>
-                    <HomeScreenCard :title="'homescreen.subjects'">
-                        <HomeScreenCourses
-                            v-for="subject in subjects"
-                            :subject="subject"
-                            :key="subject.id"
-                        />
-                    </HomeScreenCard>
+                    <SubjectsCard />
                 </v-col>
                 <v-col>
                     <HomeScreenCard :title="'homescreen.announcements'" />
@@ -55,54 +30,15 @@
 </template>
 
 <script setup lang="ts">
-import HomeScreenCard from "@/components/cards/home/HomeScreenCard.vue";
-import HomeScreenSkeletonCard from "@/components/cards/home/HomeScreenSkeletonCard.vue";
-import HomeScreenDeadlines from "@/components/buttons/HomeScreenDeadlines.vue";
-import HomeScreenCourses from "@/components/cards/home/HomeScreenCourses.vue";
+import HomeScreenCard from "@/components/home/cards/HomeScreenCard.vue";
+import SubjectsCard from "@/components/home/cards/SubjectsCard.vue";
+import DeadlinesCard from "@/components/home/cards/DeadlinesCard.vue";
 import BackgroundContainer from "@/components/BackgroundContainer.vue";
 import { useUserQuery } from "@/queries/User";
 import { useDisplay } from "vuetify";
-import { type Deadline } from "@/models/Project";
-import { ref } from "vue";
-import { useSubjectsQuery } from "@/queries/Subject"
 
 const { data: user, isLoading } = useUserQuery(null);
 const { smAndDown } = useDisplay();
-
-const {data: subjects, isLoading: isSubjectsLoading, isError: isSubjectsError} = useSubjectsQuery();
-
-const deadlines = ref<Deadline[]>([
-    {
-        project: {
-            id: 1,
-            name: "project A",
-            subject_id: 1,
-            deadline: new Date(2024, 4, 28, 23, 59),
-            description: "a description",
-        },
-        status: "none",
-    },
-    {
-        project: {
-            id: 2,
-            name: "project B",
-            subject_id: 2,
-            deadline: new Date(2024, 6, 2, 17, 0),
-            description: "another description",
-        },
-        status: "accepted",
-    },
-    {
-        project: {
-            id: 3,
-            name: "project C",
-            subject_id: 3,
-            deadline: new Date(2024, 5, 5, 22, 0),
-            description: "last description",
-        },
-        status: "rejected",
-    },
-]);
 </script>
 
 <style scoped lang="scss">
