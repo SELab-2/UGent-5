28f4f9c0296388901002ea3d0b3aab0e86424d47
Bram Reyniers <55666730+reyniersbram@users.noreply.github.com>
2024-04-19 13:45:34 +0200

    Release 2 (some additional fixes) (#148)
    * adminpanel tabel met mock data
    
    * only require certificates when running during development
    
    * quick fix
    
    * run formatter
    
    * finalize ui
    
    * i18n
    
    * use live data
    
    * toggle admin
    
    * toggle teacher
    
    * sort isTeacher and isAdmin with true first
    
    * Update frontend/src/components/project/submit/SubmitCard.vue
    
    Co-authored-by: Pieter Janin <pieter.janin@ugent.be>
    
    * Update frontend/src/components/project/submit/SubmitCard.vue
    
    Co-authored-by: Pieter Janin <pieter.janin@ugent.be>
    
    ---------
    
    Co-authored-by: Marieke <marieke.sinnaeve@ugent.be>
    Co-authored-by: Pieter Janin <pieter.janin@ugent.be>

--- frontend/src/services/user.ts
+++ frontend/src/services/user.ts
@@ -6,8 +6,16 @@ export async function getUser(uid?: string): Promise<User> {
     return authorized_fetch(`/api/users/${uid || "me"}`, { method: "GET" });
 }
 
-export async function toggleAdmin() {
-    return authorized_fetch<void>("/api/users/me", { method: "POST" });
+export async function getUsers(): Promise<User[]> {
+    return authorized_fetch("/api/users", { method: "GET" });
+}
+
+export async function toggleAdmin(uid: string): Promise<User> {
+    return authorized_fetch<User>(`/api/users/${uid}/admin`, { method: "POST" });
+}
+
+export async function toggleTeacher(uid: string): Promise<User> {
+    return authorized_fetch<User>(`/api/users/${uid}/teacher`, { method: "POST" });
 }
 
 // Fetches all subjects for logged in user
