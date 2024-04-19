d36ff05d9445d94e46ab477f2589c9ea8a607a17
Bram Reyniers <reyniersbram@gmail.com>
2024-04-19 10:29:18 +0200

    finalize ui

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -3,39 +3,75 @@
         <v-card :title="$t('admin.users')" flat class="bg-white">
             <v-card-title>
                 <v-text-field
-                    append-icon="mdi-magnify"
+                    prepend-inner-icon="mdi-magnify"
                     v-model="search"
                     label="Search"
                     single-line
                     hide-details
                 ></v-text-field>
             </v-card-title>
-            <v-data-table :headers="headers" :items="data" :search="search" class="table">
+            <v-data-table-virtual
+                :headers="headers"
+                :items="data"
+                :search="search"
+                v-model:sortBy="sortBy"
+                item-value="uid"
+                :loading="isLoading"
+                density="compact"
+                class="table"
+            >
+                <template v-slot:loading>
+                    <v-skeleton-loader type="table-row@15" class="table"></v-skeleton-loader>
+                </template>
                 <template v-slot:[`item.isTeacher`]="{ item }">
-                    <v-checkbox v-model="item.isTeacher"></v-checkbox>
+                    <v-checkbox-btn
+                        v-model="item.isTeacher"
+                        :readonly="item.uid === currentUser?.uid"
+                    ></v-checkbox-btn>
                 </template>
                 <template v-slot:[`item.isAdmin`]="{ item }">
-                    <v-checkbox v-model="item.isAdmin"></v-checkbox>
+                    <v-checkbox-btn
+                        v-model="item.isAdmin"
+                        :readonly="item.uid === currentUser?.uid"
+                    ></v-checkbox-btn>
                 </template>
-            </v-data-table>
+            </v-data-table-virtual>
         </v-card>
     </div>
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
+import { useUserQuery } from "@/queries/User";
+
+const { data: currentUser, isLoading } = useUserQuery(null);
 
 const search = ref("");
+const sortBy = ref([{key: "name", order: "asc"}]);
+
 const headers = [
-    { key: "name", title: "Name", align: "start", sortable: false },
-    { key: "email", title: "Email", sortable: false },
-    { key: "isTeacher", title: "isTeacher", sortable: false },
-    { key: "isAdmin", title: "isAdmin", sortable: false },
+    { title: "Name", key: "name", align: "start", sortable: true },
+    { title: "UGent ID", key: "uid", align: "start", sortable: false },
+    { title: "Email", key: "email", align: "start", sortable: false },
+    {
+        title: "Is Teacher",
+        key: "isTeacher",
+        sortable: true,
+        filterable: false,
+        filter: () => true, // disable filter
+    },
+    {
+        title: "Is Admin",
+        key: "isAdmin",
+        sortable: true,
+        filterable: false,
+        filter: () => true, // disable filter
+    },
 ];
 
 const data = ref([
-    { id: 0, name: "Albert", email: "albert@Ugent.be", isTeacher: false, isAdmin: false },
-    { id: 1, name: "Bert", email: "bert@ugent.be", isTeacher: true, isAdmin: false },
-    { id: 2, name: "Cedric", email: "cedrik@ugent.be", isTeacher: false, isAdmin: true },
+    { uid: "brreynie", name: "Bert", email: "bert@ugent.be", isTeacher: false, isAdmin: true },
+    { uid: "albe", name: "Albert", email: "albert@Ugent.be", isTeacher: false, isAdmin: false },
+    { uid: "cedr", name: "Cedric", email: "cedrik@ugent.be", isTeacher: false, isAdmin: true },
 ]);
 </script>
 <style scoped>
