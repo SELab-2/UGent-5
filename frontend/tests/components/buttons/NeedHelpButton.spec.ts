db4087ea5452eca0bab2733be4f22f060a79d5b5
Marieke <marieke.sinnaeve@ugent.be>
2024-05-10 20:08:34 +0200

    linter

--- frontend/tests/components/buttons/NeedHelpButton.spec.ts
+++ frontend/tests/components/buttons/NeedHelpButton.spec.ts
@@ -1,5 +1,5 @@
 import {mount} from "@vue/test-utils";
-import {expect, describe, it, vi} from "vitest";
+import {expect, describe, it} from "vitest";
 import NeedHelpButton from "@/components/buttons/NeedHelpButton.vue"
 
 describe("NeedHelpButton", () => {
