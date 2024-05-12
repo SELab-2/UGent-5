aee21d408f13a70d657bd8ce2b44b61e0b28de81
Pieter Janin <Pieter.Janin@UGent.be>
2024-05-12 17:01:38 +0200

    prune

--- backend/src/docker_tests/docker_tests.py
+++ backend/src/docker_tests/docker_tests.py
@@ -86,6 +86,8 @@ async def run_docker_tests(image_tag: str, submission_uuid: str, artifact_dir: s
         )
     except APIError as e:
         return DockerResult(status=Status.Crashed, test_results=[], stdout=None, stderr=str(e))
+    finally:
+        client.containers.prune()
 
     try:
         container.start()
