6082df3b0bb296d5d7516123fde79b15e5d77da8
Xander Bil <xander@biltopia.org>
2024-05-18 21:20:49 +0200

    Move submissions list to group page

--- frontend/src/i18n/locales/en.ts
+++ frontend/src/i18n/locales/en.ts
@@ -41,7 +41,7 @@ export default {
         files: "Files",
         download_info: "Click on filename to download",
         after_deadline: "After deadline",
-        submissions_title: "Submissions for project {project}",
+        submissions_title: "Submissions:",
         no_submissions: "No submissions yet",
         teacher_submissions_info:
             "This page contains a list of the latest submission of each group for this project.",
