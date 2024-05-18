ac97ec722bfe9fc34d1c3b01d1d1b2059291816c
mattiscauwel <mattiscauwel@gmail.com>
2024-05-18 17:10:40 +0200

    added first form elements

--- frontend/src/views/subject/CreateSubjectView.vue
+++ frontend/src/views/subject/CreateSubjectView.vue
@@ -1,6 +1,30 @@
 <template>
     <div>
         <v-form>
+            <v-responsive
+                class="mx-auto"
+                max-width="50vw"
+            >
+                <v-text-field
+                    v-model="project_title"
+                    :rules="[rules.required]"
+                    label="Title"
+                    required
+                    variant="solo-inverted"
+                    placeholder="Enter Title"
+                    hint="Enter your password to access this website"
+                    clearable
+                    hide-details="auto"
+                ></v-text-field>
+            </v-responsive>
+
+            <v-select
+                v-model="activeAcademicYear"
+                :items="academicYearItems"
+                :item-title="item => `20${item}-20${item + 1}`"
+                label="Academic Year"
+                required
+            ></v-select>
 
 
         </v-form>
@@ -9,6 +33,19 @@
 
 
 <script setup lang="ts">
+
+
+import {ref, computed} from "vue";
+import useAcademicYear from "@/composables/useAcademicYear";
+
+const project_title = ref("");
+
+const rules = {
+    required: (value: string) => !!value || "Field is required.",
+};
+
+const activeAcademicYear = ref<number>(useAcademicYear())
+const academicYearItems = [activeAcademicYear.value, activeAcademicYear.value + 1];
 
 </script>
 
