fa8e2ac8ec25fac69a1ae27e7dc5f0114c80a168
Xander Bil <xander@biltopia.org>
2024-05-20 12:58:42 +0200

    frontend: download all submissions

--- frontend/src/i18n/locales/nl.ts
+++ frontend/src/i18n/locales/nl.ts
@@ -61,6 +61,7 @@ export default {
         edit: "Bewerk project",
         submissions_list: "Alle indieningen",
         submissions_list_teacher: "Alle indieningen voor dit project",
+        submissions_zip: "Download alle indieningen",
         not_found: "Geen projecten teruggevonden.",
         finished: "Afgerond",
         not_found2: "Project niet teruggevonden",
