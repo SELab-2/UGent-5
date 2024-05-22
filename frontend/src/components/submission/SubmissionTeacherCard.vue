b831ad05e6b95ce7cbec321703648be83eee0b82
Bram Reyniers <bram.reyniers@ugent.be>
2024-05-22 22:11:51 +0200

    fix group id showing instead of num

--- frontend/src/components/submission/SubmissionTeacherCard.vue
+++ frontend/src/components/submission/SubmissionTeacherCard.vue
@@ -1,7 +1,7 @@
 <template>
-    <v-card class="parent-card">
+    <v-card v-if="!isLoading" class="parent-card">
         <v-card-title>
-            {{ $t("project.group", { number: submission.group_id }) }}
+            {{ $t("project.group", { number: group!.num }) }}
         </v-card-title>
         <SubmissionCard class="ma-3" :submission="submission" :deadline="deadline" />
         <v-divider class="divider" />
@@ -16,13 +16,16 @@
 <script setup lang="ts">
 import SubmissionCard from "@/components/submission/SubmissionCard.vue";
 import { toRefs } from "vue";
+import type Submission from "@/models/Submission";
+import { useGroupQuery } from "@/queries/Group";
 
 const props = defineProps<{
     submission: Submission;
     deadline: Date;
 }>();
-
 const { submission } = toRefs(props);
+
+const { data: group, isLoading } = useGroupQuery(() => submission.value.group_id);
 </script>
 
 <style>
